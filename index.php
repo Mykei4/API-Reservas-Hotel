02c24612d0a8bc66de8a8107391db6aa3ac55a67
Mykei4 <132678438+Mykei4@users.noreply.github.com>
2023-12-17 22:39:43 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -5,57 +5,156 @@
     $cuenta = isset($_GET['cuenta']) ? $_GET['cuenta'] : 'cliente';
     $apikey = isset($_GET['apikey']) ? $_GET['apikey'] : 'Null';
     
-    $cuenta = 'admin';
-
     $connect = conexion($db, $cuenta, $apikey);
 
 
     switch ($_SERVER["REQUEST_METHOD"]) {
+
         case "PUT":
-            $input = json_decode(file_get_contents('php://input'), true);
-            $postId = $input['id'];
-            $telefono = $input['telefono'];
-            $sql = " UPDATE alumnos SET telefono='{$telefono}' WHERE id={$postId} " ;
-            echo $sql;
-            $dbCon->exec($sql);
-            header("HTTP/1.1 200 OK");
+            $datos = json_decode(file_get_contents('php://input'), true);
+
+            if ($cuenta === 'admin') {
+                $sql = "UPDATE reservas SET Habitacion = '{$datos['habitacion']}', Cliente = '{$datos['cliente']}' , FechaEntrada='{$datos['fecha_entrada']}', FechaSalida='{$datos['fecha_salida']}' WHERE id = {$datos['reserva']}";
+                $connect->exec($sql);
+
+                header("HTTP/1.1 200 OK");
+                echo json_encode($datos);
+            } elseif ($cuenta === 'cliente') {
+                $cliente = $apikey;
+
+                $stmt = $connect->prepare("SELECT COUNT(*) as count FROM reservas WHERE Cliente = :cliente");
+                $stmt->bindParam(':cliente', $cliente);
+                $stmt->execute();
+
+                $result = $stmt->fetch();
+
+                if ($result['count'] > 0) {
+                    $sql = "UPDATE reservas SET Habitacion = '{$datos['habitacion']}', FechaEntrada = '{$datos['fecha_entrada']}', FechaSalida = '{$datos['fecha_salida']}' WHERE apikey={$datos['apikey']}";
+                    $connect->exec($sql);
+
+                    header("HTTP/1.1 200 OK");
+                    echo json_encode($datos);
+                } else {
+                    header("HTTP/1.1 403 Forbidden");
+                    echo "No tienes permiso para actualizar esta reserva";
+                }
+            } else {
+                header("HTTP/1.1 400 Bad Request");
+                echo "Tipo de cuenta no válido";
+            }
             break;
 
         case "POST":
-            $input =json_decode(file_get_contents('php://input'), true);
-            $sql = "INSERT INTO alumnos (nombre, apellido1, apellido2, telefono) VALUES ('{$input['nombre']}', '{$input['apellido1']}', '{$input['apellido2']}', '{$input['telefono']}')";
-            echo $sql;
-            $dbCon->exec($sql);
-            $aluId = $dbCon->lastInsertId();
-            if($aluId) {
-                $input['id'] = $aluId;
-                header("HTTP/1.1 200 OK");
-                echo json_encode($input);
+            $datos = json_decode(file_get_contents('php://input'), true);
+
+            if ($cuenta === 'admin') {
+                $sql = "INSERT INTO reservas (Habitacion, Cliente, FechaEntrada, FechaSalida) VALUES ('{$datos['habitacion']}', '{$datos['cliente']}', '{$datos['fecha_entrada']}', '{$datos['fecha_salida']}')";
+                
+                $connect->exec($sql);
+                $editar = $connect->lastInsertId();
+
+                if ($editar) {
+                    $datos['apikey'] = $editar;
+                    header("HTTP/1.1 200 OK");
+                    echo json_encode($datos);
+                }
+            } else if ($cuenta === 'cliente') {
+                $campos = ['Habitacion', 'FechaEntrada', 'FechaSalida'];
+                $filtrado = array_intersect_key($datos, array_flip($campos));
+
+                if (count($filtrado) === count($campos)) {
+                    $sql = "INSERT INTO reservas (Habitacion, FechaEntrada, FechaSalida) VALUES ('{$filtrado['Habitacion']}', '{$filtrado['FechaEntrada']}', '{$filtrado['FechaSalida']}')";
+
+                    $connect->exec($sql);
+                    $editar = $connect->lastInsertId();
+
+                    if ($editar) {
+                        $response = [
+                            'apikey' => $editar,
+                            'Habitacion' => $filtrado['Habitacion'],
+                            'FechaEntrada' => $filtrado['FechaEntrada'],
+                            'FechaSalida' => $filtrado['FechaSalida']
+                        ];
+                        header("HTTP/1.1 200 OK");
+                        echo json_encode($response);
+                    }
+                } else {
+                    header("HTTP/1.1 400 Bad Request");
+                    echo "No se proporcionaron todos los campos";   
+                }
+            } else {
+                header("HTTP/1.1 400 Bad Request");
+                echo "Tipo de cuenta no válido";
             }
             break;
 
         case "DELETE";
-            $input =json_decode(file_get_contents('php://input'), true);
-            $sql = $dbCon->prepare("DELETE FROM alumnos where id=:id");
-            $sql->bindValue(':id', $input['id']);
-            $sql->execute();
-            header("HTTP/1.1 200 OK");
+                $datos = json_decode(file_get_contents('php://input'), true);
+
+                if ($cuenta === 'admin') {
+                    $sql = "DELETE FROM reservas WHERE id = :reserva";
+                    $stmt = $connect->prepare($sql);
+                    $stmt->bindParam(':reserva', $datos['reserva']);
+                    $stmt->execute();
+
+                    header("HTTP/1.1 200 OK");
+                    echo "Reserva eliminada correctamente por el admin.";
+                } else if ($cuenta === 'cliente') {
+                    $cliente = $apikey;
+
+                    $stmt = $connect->prepare("SELECT COUNT(*) as count FROM reservas WHERE Cliente = :cliente AND id = :reserva");
+                    $stmt->bindParam(':cliente', $cliente);
+                    $stmt->bindParam(':reserva', $datos['reserva']);
+                    $stmt->execute();
+
+                    $result = $stmt->fetch();
+
+                    if ($result['count'] > 0) {
+                        $sql = "DELETE FROM reservas WHERE id = :reserva";
+                        $stmt = $connect->prepare($sql);
+                        $stmt->bindParam(':reserva', $datos['reserva']);
+                        $stmt->execute();
+
+                        header("HTTP/1.1 200 OK");
+                        echo "Reserva eliminada correctamente por el cliente";
+                    } else {
+                        header("HTTP/1.1 403 Forbidden");
+                        echo "No tienes reservas que eliminar";
+                    }
+                } else {
+                    header("HTTP/1.1 400 Bad Request");
+                    echo "Tipo de cuenta no válido";
+                }
             break;
 
         case "GET":
-            $sql = "SELECT * from reservas";
-            $resultado = $connect->query($sql);
-            $reserva = array();
-            $reservas = array(); 
-            while ($registro = $resultado->fetch()) {
-                $reserva["Habitacion"] = $registro["Habitacion"];
-                $reserva["Cliente"] = $registro["Cliente"];
-                $reserva["FechaEntrada"] = $registro["FechaEntrada"];
-                $reserva["FechaSalida"] = $registro["FechaSalida"];
+            if ($cuenta === 'admin') {
+                $sql = "SELECT * FROM reservas";
+            } elseif ($cuenta === 'cliente') {
+                $cliente = $apikey;
+                $sql = "SELECT * FROM reservas WHERE Cliente = :cliente";
+            } else {
+                header("HTTP/1.1 400 Bad Request");
+                echo "Tipo de cuenta no válido";
+                break;
+            }
+        
+            $stmt = $connect->prepare($sql);
+            if ($cuenta === 'cliente')
+                $stmt->bindParam(':cliente', $cliente);
+            $stmt->execute();
 
-                $reservas[$registro["id"]] = $reserva;
+            $reservas = array();
+            while ($registro = $stmt->fetch()) {
+                $reservas[$registro["id"]] = [
+                    "Habitacion" => $registro["Habitacion"],
+                    "Cliente" => $registro["Cliente"],
+                    "FechaEntrada" => $registro["FechaEntrada"],
+                    "FechaSalida" => $registro["FechaSalida"]   
+                ];
             }
             echo json_encode($reservas);
+
             break;
 
         default:
@@ -66,10 +165,10 @@
 
     function conexion($db, $cuenta, $apikey) {
         if ($cuenta === 'cliente') {
-            $pdo = new PDO("mysql:host={$db['admin']['host']};dbname={$db['admin']['db']};charset=utf8", $db['admin']['username'], $db['admin']['password']);
+            $pdoCliente = new PDO("mysql:host={$db['admin']['host']};dbname={$db['admin']['db']};charset=utf8", $db['admin']['username'], $db['admin']['password']);
     
             $sql = "SELECT COUNT(*) as count FROM usuarios WHERE APIKEY = :apiKey";
-            $stmt = $pdo->prepare($sql);
+            $stmt = $pdoCliente->prepare($sql);
             $stmt->bindParam(':apiKey', $apiKey);
             $stmt->execute();
     
@@ -82,8 +181,21 @@
                 echo 'APIKEY inválido. Acceso denegado.';
             }
         } else if ($cuenta === 'admin') {
-            $db = $db['admin'];
-            return new PDO("mysql:host={$db['host']};dbname={$db['db']};charset=utf8", $db['username'], $db['password']);
+            $pdoAdmin = new PDO("mysql:host={$db['admin']['host']};dbname={$db['admin']['db']};charset=utf8", $db['admin']['username'], $db['admin']['password']);
+            
+            $sql = "SELECT Permisos FROM usuarios WHERE APIKEY = :apiKey AND Permisos = 1";
+            $stmt = $pdoAdmin->prepare($sql);
+            $stmt->bindParam(':apiKey', $apikey);
+            $stmt->execute();
+    
+            $result = $stmt->fetch();
+            
+            if ($result) {
+                $db = $db['admin'];
+                return new PDO("mysql:host={$db['host']};dbname={$db['db']};charset=utf8", $db['username'], $db['password']);
+            } else {
+                echo 'Acceso denegado.';
+            }
         } else {
             echo 'Tipo de cuenta NO valido.';
         }
